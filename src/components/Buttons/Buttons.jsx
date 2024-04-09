ae1d0c2a6a7d84e8e7b66b60edb1d001369f134a
Fahid <111286245+Fahid08@users.noreply.github.com>
2024-04-09 12:45:55 +0500

    Update Buttons.jsx

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -4,7 +4,8 @@ import { Spin } from "antd";
 import GetTableInfo from "../Table/GetTableInfo";
 import { Input, Form, Select } from "antd";
 import "./style.css";
-const baseUrl = "http://127.0.0.1:8000/invocations/executesql";
+// const baseUrl = "http://127.0.0.1:8000/invocations/executesql";
+const baseUri = "https://uj7fyddl6oea72cu576ogfu6vu0qxhmu.lambda-url.us-east-1.on.aws/executequery";
 const { Option } = Select;
 function Buttons({
   setCSVData,
