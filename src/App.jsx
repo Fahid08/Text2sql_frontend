b97f63420c4be20cd04682762be7107c53959f68
Fahid <111286245+Fahid08@users.noreply.github.com>
2024-04-19 13:19:42 +0500

    changed aliabbas latest src files to main src

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import LayoutContainer from '../src/components/Layout'
+import Guide from '../src/components/Guide'
+import {Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
 const App = () => {
   return (
     <div>
-      <LayoutContainer/>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<LayoutContainer/>}/>
+        <Route path='/how-to-use' element={<Guide/>}/>
+      </Routes>
     </div>
   )
 }
